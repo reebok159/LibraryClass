60458cac681d7d0988f585cd172daa6732fba186
Eugene V <kokokoi9@mail.ru>
2017-05-29 22:30:00 +0300

    small changes

--- classes/library.rb
+++ classes/library.rb
@@ -23,53 +23,48 @@ class Library
       next unless instance_variable_defined?( str_name.to_sym )
       arrayName = eval(str_name)
       if arrayName
-
-
         self.addItem(item.author) if item.is_a?(Book)
 
         if item.is_a?(Order)
           self.addItem(item.book)
-          self.addItem(item.reader)
+         self.addItem(item.reader)
         end
 
-        is_exists = arrayName.include?(item)
-        arrayName << item if !is_exists
+        arrayName << item if !arrayName.include?(item)
       end
     end
   end
-
 
   def getOftenReader
     return "not found" if orders.size == 0
     result = @orders.group_by{ |item| item.reader.id }.sort_by{ |id_reader, reader| -reader.count }
     result.map!{ |item| item[1] }
-
     result[0].first.reader
   end
 
-
   def mostPopularBook
     return "not found" if orders.size == 0
-    result = @orders.group_by{ |item| item.book.id }.sort_by{ |id_book, book| -book.count }
-    result.map!{ |item| item[1] }
-
+    result = getGroupedOrders
     result[0].first.book
   end
 
   def howManyGet3PopularBooks
     return 0 if orders.size == 0
-    grouped_orders = @orders.group_by{ |item| item.book.id }.sort_by{ |id_book, book| -book.count }
-    grouped_orders.map!{ |item| item[1] }
-
+    grouped_orders = getGroupedOrders
     three_books_in_groups = grouped_orders.first(3)
-
     common_group = []
 
     three_books_in_groups.each {|item| common_group |= item }
     total = common_group.uniq{ |item| item.reader.id }.count
-
     total
   end
+
+  def getGroupedOrders
+    result = @orders.group_by{ |item| item.book.id }.sort_by{ |id_book, book| -book.count }
+    result.map!{ |item| item[1] }
+    result
+  end
+
 
   def saveLibrary(path = './data/lib.yml')
     begin
